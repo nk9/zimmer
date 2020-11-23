e5677b3547f315dff58dec1519cebffaa686b096
Nick Kocharhook <nick@kocharhook.com>
2020-11-23 22:22:05 +0000

    Lego pieces snap to grid. Fix multiple bricks aligned.

--- zimmer.js
+++ zimmer.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser'
 
-var brick;
+var bricks = [];
+const kDragThreshold = 2;
+const kLegoGrid = 29;
 
 function preload() {
 	// this.load.setBaseURL('http://labs.phaser.io')
@@ -19,28 +21,41 @@ function create() {
     helloButton.setInteractive();
     helloButton.on('pointerover', () => { console.log('pointerover'); });
 
-    brick = this.add.sprite(100,100, 'yellow-bricks', '2x4.png')
-    brick.setOrigin(0.5);
-    brick.setInteractive()
-    	.on('pointerover', () => { console.log('brick!'); })
-    	.on('pointerup', pointer => { if (pointer.getDistance() < 1) { brick.angle += 90; } });
-    this.input.setDraggable(brick);
-    this.input.dragDragThreshold = 1;
+    // Create bricks
+    addBrick.call(this, '2x4', 5, 6);
+    addBrick.call(this, '1x4', 10, 6);
+    addBrick.call(this, '1x1', 2, 6);
+
+    this.input.dragDragThreshold = kDragThreshold;
 
     this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-
-        gameObject.x = dragX;
-        gameObject.y = dragY;
-
+        gameObject.x = Phaser.Math.Snap.To(dragX, kLegoGrid);
+        gameObject.y = Phaser.Math.Snap.To(dragY, kLegoGrid);
     });
 
 	// var under_bridge = this.add.image(0, 0, 'under_bridge')
 	// under_bridge.setOrigin(0,0)
 }
 
+function addBrick(brickName, x, y) {
+	let brick = this.add.sprite(x * kLegoGrid,
+								y * kLegoGrid,
+								'yellow-bricks',
+								brickName+".png")
+	brick.setOrigin(0, 0);
+	brick.setInteractive()
+		// .on('pointerover', () => { console.log('brick!'); })
+		.on('pointerup', pointer => { if (pointer.getDistance() < kDragThreshold) { brick.angle += 90; } });
+	this.input.setDraggable(brick);
+
+	bricks.push(brick);
+}
+
 function update() {
 	// Has to be called in update in order for the change to register
-	brick.angle = brick.angle;
+	for (var i = bricks.length - 1; i >= 0; i--) {
+		bricks[i].angle = bricks[i].angle;
+	}
 }
 
 const config = {
