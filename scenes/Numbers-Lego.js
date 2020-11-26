49e50e5e210b32b942005dce4a8dd13e25099ec1
Nick Kocharhook <nick@kocharhook.com>
2020-11-26 22:29:01 +0000

    Get pouch tweens working. Use a timeline.

--- scenes/Numbers-Lego.js
+++ scenes/Numbers-Lego.js
@@ -8,6 +8,10 @@ var pouch_open;
 const LEGO_GRID = 29;
 const SMALL_POUCH_X = 1000;
 const SMALL_POUCH_Y = 700;
+const BEHIND_BACKGROUND_DEPTH = -2;
+const POUCH_DEPTH = 10;
+const UNDER_POUCH_DEPTH = 8;
+const ABOVE_POUCH_DEPTH = 12;
 
 class Numbers_Lego extends BaseScene {
     constructor() {
@@ -46,6 +50,7 @@ class Numbers_Lego extends BaseScene {
 	        gameObject.x = Phaser.Math.Snap.To(dragX, LEGO_GRID);
 	        gameObject.y = Phaser.Math.Snap.To(dragY, LEGO_GRID);
 	    });
+	    this.createBricks();
 	}
 
 	update() {
@@ -53,6 +58,21 @@ class Numbers_Lego extends BaseScene {
 		// for (var i = bricks.length - 1; i >= 0; i--) {
 		// 	bricks[i].angle = bricks[i].angle;
 		// }
+	}
+
+	createBricks(tween, targets, custom) {
+		console.log("createBricks");
+
+	    // Create bricks
+	    const offset = 15 * LEGO_GRID;
+	    this.addBrick('1x2', 29, 3);
+	    this.addBrick('1x2', 32, 3);
+	    this.addBrick('1x2', 35, 3);
+	    this.addBrick('2x4', 29, 5);
+	    this.addBrick('2x4', 34, 5);
+	    this.addBrick('2x2', 29, 8);
+	    this.addBrick('2x3', 32, 8);
+	    this.addBrick('2x3', 29, 18);
 	}
 
 	addBrick(brickName, x, y) {
@@ -66,6 +86,7 @@ class Numbers_Lego extends BaseScene {
 			.on('pointerup', pointer => { if (pointer.getDistance() < DRAG_THRESHOLD) { brick.angle += 90; } });
 		this.input.setDraggable(brick);
 
+		brick.setDepth(BEHIND_BACKGROUND_DEPTH);
 		bricks.push(brick);
 	}
 
@@ -74,44 +95,37 @@ class Numbers_Lego extends BaseScene {
 	    
 	    pouch_open = this.add.image(SMALL_POUCH_X, SMALL_POUCH_Y, 'pouch_open');
 	    pouch_open.scale=0.1;
-	    var tween = this.tweens.add({
-	    	targets: pouch_open,
-	    	scale: .8,
-	    	x: 36 * LEGO_GRID,
-	    	y: 12 * LEGO_GRID,
-	    	ease: 'Sine.easeOut',
-	    	duration: 1000,
-	    	onComplete: this.showBricks,
-	    	onCompleteScope: this
-	    });
+
+	    var timeline = this.tweens.timeline({
+	    	tweens: [{
+				targets: pouch_open,
+				scale: .8,
+				x: 36 * LEGO_GRID,
+				y: 13 * LEGO_GRID,
+				ease: 'Sine.easeOut',
+				duration: 1000,
+				onComplete: function (tween, sprite) {
+					pouch_open.setDepth(POUCH_DEPTH);
+				}
+	    	}, {
+				targets: bricks.slice().reverse(),
+				y: -5*LEGO_GRID,
+				yoyo: true,
+				repeat: 0,
+				ease: 'Sine.easeOut',
+				duration: 350,
+				onStart: function (tween, targets) {
+					for (var i = targets.length - 1; i >= 0; i--) {
+						targets[i].setDepth(UNDER_POUCH_DEPTH); // Move bricks on top of background, behind pouch
+					}
+				},
+				onYoyo: function (tween, sprite) {
+					sprite.setDepth(ABOVE_POUCH_DEPTH); // Move bricks on top of pouch
+				},
+				delay: function(target, targetKey, value, targetIndex, totalTargets, tween) {
+					return targetIndex * Phaser.Math.Between(0, 250);
 	}
-
-	showBricks(tween, targets, custom) {
-		console.log("showBricks");
-
-	    // Create bricks
-	    const offset = 15 * LEGO_GRID;
-	    this.addBrick('1x2', 29, 2);
-	    this.addBrick('1x2', 32, 2);
-	    this.addBrick('1x2', 35, 2);
-	    this.addBrick('2x4', 29, 4);
-	    this.addBrick('2x4', 34, 4);
-	    this.addBrick('2x2', 29, 7);
-	    this.addBrick('2x3', 32, 7);
-	    this.addBrick('2x3', 29, 10);
-
-		pouch_open.setDepth(1);
-
-	    var tween = this.tweens.add({
-	    	targets: bricks,
-	    	y: '-='+offset,
-	    	yoyo: true,
-	    	repeat: 0,
-	    	ease: 'Bounce.easeOut',
-	    	duration: 500,
-	    	onYoyo: function (tween, sprite) {
-	    		sprite.setDepth(2);
-	    	}
+	    	}]
 	    });
 	}
 }
