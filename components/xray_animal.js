c89bb4ae4a5c573ef9eff37d94c4eb86eb2a5be1
Nick Kocharhook <nick@kocharhook.com>
2021-01-02 15:05:12 +0000

    Center x-ray image.

--- components/xray_animal.js
+++ components/xray_animal.js
@@ -11,8 +11,8 @@ class XrayAnimal extends OutlineTargetImage {
 
 		super(scene, name, targetX, targetY, randomPoint.x, randomPoint.y, scale);
 
-		this.xrayImg = scene.add.image(910, 460, name+'_xray');
-		this.xrayImg.setOrigin(0, 0);
+		this.xrayImg = scene.add.image(1025, 460, name+'_xray');
+		this.xrayImg.setOrigin(0.5, 0);
 		this.xrayImg.visible = false;
 		this.success = success;
 	}
